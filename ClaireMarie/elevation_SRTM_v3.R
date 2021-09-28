bd1ba6a1ce7841086e4ac8dd2177dcf9e3502fd5
jon <jon.yearsley@ucd.ie>
2021-09-28 11:46:58 +0100

    Modift digital elevation processing

--- ClaireMarie/elevation_SRTM_v3.R
+++ ClaireMarie/elevation_SRTM_v3.R
@@ -15,6 +15,10 @@
 #8. Display slope
 #8. Save data
 #
+# Adapted by Jon Yearsley
+# 28th Sept 2021
+#  Outputs elevation for all squares across all of Ireland
+#  Only produces graphics on request (plot_on=TRUE)
 # ****************************************
 
 
@@ -26,22 +30,35 @@ library(ggplot2)
 library(raster)
 
 
-datadir = '~/Stage/Data/MODIS/Phenophase_estimates'
-quadratPath = '~/Stage/Data/Quadrats'
-outputDir = '~/Stage/Data_created/elevation_SRTM3_square'
+# datadir = '~/Stage/Data/MODIS/Phenophase_estimates'
+# quadratPath = '~/Stage/Data/Quadrats'
+# outputDir = '~/Stage/Data_created/elevation_SRTM3_square'
+# 
+# SRTM3Path = '~/Stage/Data/DigitalElevation/SRTM_data/hgt'
 
-SRTM3Path = '~/Stage/Data/DigitalElevation/SRTM_data/hgt'
+#datadir = '~/Stage/Data/MODIS/Phenophase_estimates'
+modis_dir = '/Volumes/MODIS_data/MODIS/'
+quadratPath = '/Volumes/MODIS_data/Quadrats/'
+outputDir = '/Volumes/MODIS_data/Elevation_SRTM_squares/'
 
-input_file_prefix = 'phenology'
+SRTM3Path = '/Volumes/MODIS_data/Elevation_SRTM_squares/hgt/'
+
+plot_on = FALSE
+
+
+# input_file_prefix = 'phenology'
 
 setwd(SRTM3Path)
 
 # List of squares to analyse
-squareList = c(1:9, 13:19, 21)
-year = 2019
+squareList = c(10:12)
+# year = 2019
 
 
 # SRTM3 data : WGS84
+files = list.files(pattern='.hgt')
+SRTM = st_mosaic(files)
+
 SRTM3 = st_mosaic(read_stars("N51W009.hgt", NA_value = 0), 
                   read_stars("N51W010.hgt", NA_value = 0),
                   read_stars("N52W007.hgt", NA_value = 0), 
@@ -68,20 +85,20 @@ crs_squares = st_crs(squares)
 squares_gtopo = st_transform(squares, crs = crs_SRTM3)
 
 # Read in MODIS grid
-modis = read_stars('~/Stage/Data/MODIS/modis_grid_ireland.tif')
+modis = read_stars(file.path(modis_dir,'modis_grid_ireland.tif'))
 st_crs(modis) =  crs_squares  # Make sure modis and squares have same CRS
 
-nb_pixels = data.frame()
+# nb_pixels = data.frame()
 
 # Elevation data by squares
 for (s in squareList) {
   
-  # Import phenophase estimate
-  filename = paste0(input_file_prefix,'_square_',s,'_',year,'.RData')
-  load(file.path(datadir,filename))
-  
-  pixel_list = unique(d_final$pixelID)
-  nPixel = length(pixel_list)
+  # # Import phenophase estimate
+  # filename = paste0(input_file_prefix,'_square_',s,'_',year,'.RData')
+  # load(file.path(datadir,filename))
+  # 
+  # pixel_list = unique(d_final$pixelID)
+  # nPixel = length(pixel_list)
   
   # Crop modis grid to a square and then resample elevation onto this cropped grid
   crop_square = st_crop(modis, squares[s,])
@@ -96,94 +113,98 @@ for (s in squareList) {
   }
   write_stars(adrop(crop_modis), file.path(outputDir,fname))
   
-  # Use the field function
-  crop_slope = raster(file.path(outputDir,fname))
-  crop_slope = terrain(crop_slope, opt='slope', unit='degrees')
   
-  
-  tmp = data.frame()
-  
-  for (i in pixel_list) {
-    print(i)
+  if (plot_on) {
+    # Visualise some of the data ----------
+    # Use the field function
+    crop_slope = raster(file.path(outputDir,fname))
+    crop_slope = terrain(crop_slope, opt='slope', unit='degrees')
     
-    # Retrieve the index of one of the pixels
-    ind = which(d_final$pixelID == i)[1]
     
-    # Create a modis geometry column
-    d_final$geometry[ind] = st_sfc(st_point(c(d_final$x_MODIS[ind], 
-                                              d_final$y_MODIS[ind])))
-    # Create a elevation colomn thanks to geometry
-    d_final$elevation[ind] = as.numeric(st_extract(crop_modis, 
-                                                   st_sfc(d_final$geometry[ind], 
-                                                          crs = st_crs(modis))))
-    # Create a slope colomn
-    d_final$slope[ind] = as.numeric(st_extract(st_as_stars(crop_slope), 
-                                               st_sfc(d_final$geometry[ind], 
-                                                      crs = st_crs(modis))))
+    tmp = data.frame()
     
-    tmp = rbind(tmp, d_final[ind,])
-    nb_pixels = rbind(nb_pixels, d_final[ind,])
-
+    for (i in pixel_list) {
+      print(i)
+      
+      # Retrieve the index of one of the pixels
+      ind = which(d_final$pixelID == i)[1]
+      
+      # Create a modis geometry column
+      d_final$geometry[ind] = st_sfc(st_point(c(d_final$x_MODIS[ind], 
+                                                d_final$y_MODIS[ind])))
+      # Create a elevation colomn thanks to geometry
+      d_final$elevation[ind] = as.numeric(st_extract(crop_modis, 
+                                                     st_sfc(d_final$geometry[ind], 
+                                                            crs = st_crs(modis))))
+      # Create a slope colomn
+      d_final$slope[ind] = as.numeric(st_extract(st_as_stars(crop_slope), 
+                                                 st_sfc(d_final$geometry[ind], 
+                                                        crs = st_crs(modis))))
+      
+      tmp = rbind(tmp, d_final[ind,])
+      nb_pixels = rbind(nb_pixels, d_final[ind,])
+      
+    }
+    
+    # Min, max elevations values
+    min = min(tmp$elevation, na.rm=T)
+    max = max(tmp$elevation, na.rm=T)
+    
+    # Display elevation
+    ggplot(data=tmp,
+           aes(x=x_MODIS,
+               y=y_MODIS,
+               fill=elevation)) +
+      geom_tile(colour='darkblue') +
+      coord_equal() +
+      scale_fill_gradientn(colours = terrain.colors(20),
+                           na.value='darkgray') +
+      labs(x='X Coord (MODIS CRS)',
+           y='Y Coord (MODIS CRS)',
+           title=paste('Elevation SRTM3 square', s),
+           subtitle = paste('min = ', min, 'm max = ', max, 'm')) +
+      theme_bw()
+    
+    # Save file as png
+    #ggsave(width=11, height=6,
+    #       filename = paste0(outputDir, '/elevation_SRTM3_square_',s,'.png'))
+    
+    
+    # Min, max slopes values
+    mins = min(tmp$slope, na.rm=T)
+    maxs = max(tmp$slope, na.rm=T)
+    
+    # Display slope
+    ggplot(data=tmp,
+           aes(x=x_MODIS,
+               y=y_MODIS,
+               fill=slope)) +
+      geom_tile(colour='darkblue') +
+      coord_equal() +
+      scale_fill_gradientn(colours = terrain.colors(20),
+                           na.value='darkgray') +
+      labs(x='X Coord (MODIS CRS)',
+           y='Y Coord (MODIS CRS)',
+           title=paste('Slope square', s),
+           subtitle = paste('min = ', mins, '? max = ', maxs, '?')) +
+      theme_bw()
+    
+    # Save file as png
+    ggsave(width=11, height=6,
+           filename = paste0(outputDir, '/slope_elevation_square_',s,'.png'))
+    
+    
+    
+    # Display the number of pixels of same elevation
+    ggplot(data=tmp, aes(x = elevation)) +
+      geom_histogram(bins=30) +
+      labs(x='Elevation',
+           y = 'Number of Pixels',
+           title=paste('Elevation square ',s))
+    
+    #ggsave(width=11, height=6,
+    #       filename = paste0(outputDir, '/nb_pixels_square_',s,'.png'))
   }
-  
-  # Min, max elevations values
-  min = min(tmp$elevation, na.rm=T)
-  max = max(tmp$elevation, na.rm=T)
-  
-  # Display elevation
-  ggplot(data=tmp,
-         aes(x=x_MODIS,
-             y=y_MODIS,
-             fill=elevation)) +
-    geom_tile(colour='darkblue') +
-    coord_equal() +
-    scale_fill_gradientn(colours = terrain.colors(20),
-                         na.value='darkgray') +
-    labs(x='X Coord (MODIS CRS)',
-         y='Y Coord (MODIS CRS)',
-         title=paste('Elevation SRTM3 square', s),
-         subtitle = paste('min = ', min, 'm max = ', max, 'm')) +
-    theme_bw()
-  
-  # Save file as png
-  #ggsave(width=11, height=6,
-  #       filename = paste0(outputDir, '/elevation_SRTM3_square_',s,'.png'))
-  
-  
-  # Min, max slopes values
-  mins = min(tmp$slope, na.rm=T)
-  maxs = max(tmp$slope, na.rm=T)
-  
-  # Display slope
-  ggplot(data=tmp,
-         aes(x=x_MODIS,
-             y=y_MODIS,
-             fill=slope)) +
-    geom_tile(colour='darkblue') +
-    coord_equal() +
-    scale_fill_gradientn(colours = terrain.colors(20),
-                         na.value='darkgray') +
-    labs(x='X Coord (MODIS CRS)',
-         y='Y Coord (MODIS CRS)',
-         title=paste('Slope square', s),
-         subtitle = paste('min = ', mins, '° max = ', maxs, '°')) +
-    theme_bw()
-  
-  # Save file as png
-  ggsave(width=11, height=6,
-         filename = paste0(outputDir, '/slope_elevation_square_',s,'.png'))
-
-  
-  
-  # Display the number of pixels of same elevation
-  ggplot(data=tmp, aes(x = elevation)) +
-    geom_histogram(bins=30) +
-    labs(x='Elevation',
-         y = 'Number of Pixels',
-         title=paste('Elevation square ',s))
-  
-  #ggsave(width=11, height=6,
-  #       filename = paste0(outputDir, '/nb_pixels_square_',s,'.png'))
 }
 
 # Display the number total of pixels of same elevation
--- ClaireMarie/slope_aspect_elevation_JY.R
+++ ClaireMarie/slope_aspect_elevation_JY.R
@@ -1,7 +1,8 @@
-# Slope aspect of SRTM elevation
+# Put all slope, aspect, soil data together into one data frame
 #
-# Claire-Marie Alla
-# 29/06/2021
+# Claire-Marie Alla  & Jon Yearsley (Jon.Yearsley@ucd.ie)
+# Started 29/06/2021
+# Last edited 27/09/2021
 # ++++++++++++++++++++++++++++++++++++++++++++++
 
 rm(list=ls())
@@ -13,78 +14,118 @@ library(terra)
 library(ggplot2)
 
 elevation_dir = '/Volumes/MODIS_data/Elevation_SRTM_squares/'
-# modisPath = '~/Stage/Data/MODIS'
-# dataDir = '~/Stage/Data/MODIS/Phenophase_estimates'
-# outputDir = '~/Stage/Data_created'
+soil_file =  '/Volumes/MODIS_data/Soils_IE_WetDry/Soils_IE_WetDry.shp'
 
-# input_file_preffix = 'phenology'
+outputDir = '/Volumes/MODIS_data/Data_created/'
+squaresList = c(1:21) 
+plot_on=FALSE
+
 
 # Import data --------
-squaresList = c(18)
 
 
-# 
-# # Method 1
-# for (e in 1:length(squaresList)) {
-#   filename = paste0('square_',squaresList[e],'.tif')
-#   square = read_stars(file.path(elevation_dir, filename))
-#   
-#   filename2 = paste0(input_file_preffix,'_square_',squaresList[e],'_',year,'.RData')
-#   load(file.path(dataDir,filename2))
-#   
-#   c = st_contour(square, breaks=c(100, 150))
-#   print(c)
-#   
-#   # Create the gradient from the outline ? 
-#   # But we don't have the expression of the fonction
-# }
+# Read soil data as a SpatVector object (terra package)
+soil = terra::vect(x=soil_file)
+
+# Create a categorical raster layer for the soil types
+soil_cat = unique(soil$CATEGORY)
+cat_df = data.frame(ID = c(1:length(soil_cat)-1), 
+                    CATEGORY = soil_cat)
+soil$ID = cat_df$ID[match(soil$CATEGORY, cat_df$CATEGORY)]
+
+# Reproject onto MODIS CRS
+tmp = rast(file.path(elevation_dir, 'square_1.tif'))
+soil_modis = project(soil, tmp)
 
 # Method 1 (JY version)
 for (e in 1:length(squaresList)) {
   filename = paste0('square_',squaresList[e],'.tif')
   elevation = rast(file.path(elevation_dir, filename))
+  names(elevation) = "elevation"
   slope = terrain(elevation, v="slope", unit="degrees", neighbors=8)
   aspect = terrain(elevation, v="aspect", unit="degrees", neighbors=8)
+  
+  if (squaresList[e]%in%c(10:12)) { 
+    # No soil data for Northern Ireland
+    square_rast = c(elevation, slope, aspect)
+    square_rast$SOIL_TYPE = -9
+    
+  } else {
+    tmp = crop(soil_modis, elevation)
+    soil_rast = rasterize(tmp, elevation, field="ID", fun="last")
+    levels(soil_rast) = cat_df$CATEGORY
+    names(soil_rast) = "SOIL_TYPE"
+
+    square_rast = c(elevation, slope, aspect, soil_rast)
+  }
+  square_rast$SQUARE_ID = squaresList[e]
+  
+  if (e==1) {
+    all_squares = square_rast
+  } else {
+    all_squares = merge(all_squares, square_rast)
+  }
+  
+  writeRaster(square_rast, filename=file.path(outputDir,filename), overwrite=TRUE)
 }
 
 
-# PLot elevation
-ggplot() + 
-  geom_stars(data=st_as_stars(elevation)) +
-  coord_equal() + 
-  theme_void() +
-  scale_fill_gradientn("Elevation (m)",colours = terrain.colors(10))
-ggsave("elevation_square18.png", width=6, height=5, units="cm")
+# Write all squares in one raster file
+all_squares$SOIL_TYPE[all_squares$SOIL_TYPE==-9] = NA
+writeRaster(all_squares, filename=file.path(outputDir,'combined_data.tif'), overwrite=TRUE)
 
+# Write data as a data frame
+all_squares_df = as.data.frame(all_squares, xy=TRUE)
+names(all_squares_df) = c('x_MODIS','y_MODIS','ELEVATION','SLOPE','ASPECT','SOIL_TYPE','SQUARE_ID')
+all_squares_df$SOIL_TYPE = as.factor(cat_df$CATEGORY[match(all_squares_df$SOIL_TYPE, cat_df$ID)])
+all_squares_df$SQUARE_ID = as.factor(all_squares_df$SQUARE_ID)
+all_squares_df$SLOPE_PERCENT = slope_percent = atan(all_squares_df$SLOPE*pi/180)*100
 
-slope_percent = atan(slope*pi/180)*100
-# Plot slope
-ggplot() + 
-  geom_stars(data=st_as_stars(slope_percent)) +
-  coord_equal() + 
-  theme_void() +
-  scale_fill_viridis_c("Slope", 
-                       option = "magma",
-                       breaks=c(2,4,6,8), 
-                       labels=c("2%","4%","6%","8%"))
-ggsave("slope_square18.png", width=6, height=5, units="cm")
+save(all_squares_df, file = file.path(outputDir,'all_envData.RData'))
 
 
 
-tmp = aspect
-tmp[slope_percent<1] = NA
-
-# Plot elevation
-ggplot() + 
-  geom_stars(data=st_as_stars(tmp)) +
-  coord_equal() + 
-  scale_fill_gradient2("Aspect\n(slope>1%)", 
-                       low = 'purple4', 
-                       mid = "yellow", 
-                       high = 'purple4', 
-                       midpoint=180,
-                       breaks=c(1,90,180,270,359),
-                       labels = c("N","E","S","W","N")) +
-  theme_void() 
-
-ggsave("aspect_square18.png", width=6, height=5, units="cm")
+# Plot some of the data ------------
+if (plot_on) {
+  # PLot elevation
+  ggplot() + 
+    geom_stars(data=st_as_stars(elevation)) +
+    coord_equal() + 
+    theme_void() +
+    scale_fill_gradientn("Elevation (m)",colours = terrain.colors(10))
+  ggsave("elevation_square18.png", width=6, height=5, units="cm")
+  
+  
+  slope_percent = atan(slope*pi/180)*100
+  # Plot slope
+  ggplot() + 
+    geom_stars(data=st_as_stars(slope_percent)) +
+    coord_equal() + 
+    theme_void() +
+    scale_fill_viridis_c("Slope", 
+                         option = "magma",
+                         breaks=c(2,4,6,8), 
+                         labels=c("2%","4%","6%","8%"))
+  ggsave("slope_square18.png", width=6, height=5, units="cm")
+  
+  
+  
+  tmp = aspect
+  tmp[slope_percent<1] = NA
+  
+  # Plot elevation
+  ggplot() + 
+    geom_stars(data=st_as_stars(tmp)) +
+    coord_equal() + 
+    scale_fill_gradient2("Aspect\n(slope>1%)", 
+                         low = 'purple4', 
+                         mid = "yellow", 
+                         high = 'purple4', 
+                         midpoint=180,
+                         breaks=c(1,90,180,270,359),
+                         labels = c("N","E","S","W","N")) +
+    theme_void() 
+  
+  ggsave("aspect_square18.png", width=6, height=5, units="cm")
+  
+}
